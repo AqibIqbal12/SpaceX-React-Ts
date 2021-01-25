79e59181d16b87561c880ac33404797cd43c341d
Aqib_Iqbal <aqibiqbal48@gmail.com>
2021-01-25 14:08:40 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Routes from './RoutesConfig';
+import { ApolloProvider } from "@apollo/client";
+import client from './config/gqlconfig'
 import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+     //minHeight: "100vh",
+     display: "flex",
+     flexDirection: "column",
+     //justifyContent: "space-between",
+     position:"relative",
+    //backgroundColor:"red",
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.mainContainer}>
+      <ApolloProvider client={client}>
+        <Routes />
+      </ApolloProvider>
+
     </div>
   );
 }
