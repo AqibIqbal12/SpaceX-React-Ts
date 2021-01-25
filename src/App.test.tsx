2d9dde4678c26d0483fabeff76f3e7961d8ddd6f
Aqib_Iqbal <aqibiqbal48@gmail.com>
2021-01-25 14:08:35 +0500

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should renders a div', () => {
+  const element = render(<App />);
+  expect(element.container.getElementsByTagName("div").length).toBeGreaterThan(1);
 });
+
